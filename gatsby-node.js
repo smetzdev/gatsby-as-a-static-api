766c9e12e11ddb21d368deafa86b52a56f691485
Sascha Metz <metz@raum3.de>
2020-07-27 16:46:09 +0200

    Add json example

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,37 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// We need the filesystem to write the JSON file
+const fs = require("fs")
 
-// You can delete this file if you're not using it
+// Invoked after build has finished
+exports.onPostBuild = async ({ graphql, reporter }) => {
+  // Query all f1teams, message in the console if errors occur
+  const result = await graphql(`
+    query F1Teams {
+      allFile(
+        filter: { absolutePath: { regex: "/f1-teams/" }, ext: { eq: ".md" } }
+      ) {
+        nodes {
+          childMarkdownRemark {
+            frontmatter {
+              title
+              teamPrincipal
+              homeBase
+              logo {
+                publicURL
+              }
+              drivers
+            }
+          }
+        }
+      }
+    }
+  `)
+  if (result.errors) reporter.panic("🚨 F1Teams-query failed onPostBuild")
+
+  // Destructure result as f1Teams
+  const f1Teams = result.data.allFile.nodes.map(teamWithMarkdown => ({
+    ...teamWithMarkdown.childMarkdownRemark.frontmatter,
+  }))
+
+  // Write the JSON file
+  fs.writeFileSync("./public/f1-teams.json", JSON.stringify(f1Teams))
+}
